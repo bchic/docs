0b396911bba86cf0abad03310699336b6f3d6b75
leander <kontakt@bchic.studio>
2025-12-11 03:16:13 +0100

    New Docs

--- bchic.js
+++ bchic.js
@@ -1,4 +1,3 @@
-/*
 (function() {
     // Create a new script element
     var script = document.createElement('script');
@@ -11,30 +10,5 @@
     script.setAttribute('data-website-id', 'ee491184-2c23-4d95-85bf-0dd28f37635a');
 
     // Append the script to the head of the document to start loading it
-    document.head.appendChild(script);
-})();*/
-
-
-// bchic.js (Finaler Test-Code für localhost und Deployment)
-
-console.log("✅ bchic.js wurde geladen und wird jetzt ausgeführt!");
-
-(function() {
-    // Robuster Code zum Abfangen der Events
-    window.bchic = window.bchic || function() {
-        (window.bchic.q = window.bchic.q || []).push(arguments);
-    };
-
-    window.bchic.event = function(eventPayload) {
-        console.log('[bchic Test] Mintlify Event empfangen:', eventPayload);
-    };
-
-    // Dein ursprünglicher Code zum Laden des Haupt-Analytics-Skripts
-    var script = document.createElement('script');
-    script.src = 'https://analytics.bchic.de/script.js';
-    script.defer = true;
-
-    script.setAttribute('data-website-id', 'ee491184-2c23-4d95-85bf-0dd28f37635a');
-
     document.head.appendChild(script);
 })();
